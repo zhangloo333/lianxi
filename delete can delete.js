c81586f8c718ba731e40ce1e5aebcf6964f0dcc9
ebookcode123 <ebookcode123@gmail.com>
2017-05-17 16:58:03 -0700

    updates

--- delete can delete.js
+++ delete can delete.js
@@ -1,3 +1,14 @@
 /**
  * Created by leizha on 5/17/17.
  */
+var myObject = {
+    price: 20.99,
+    get_price : function() {
+        return this.price;
+    }
+};
+var customObject = Object.create(myObject);
+customObject.price = 19.99;
+
+delete customObject.price;
+console.log(customObject.get_price());
