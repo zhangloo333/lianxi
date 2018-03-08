f48ff6b83125de60876b490e15f82c8f6c04191c
Lee Z <zhanglee666@gmail.com>
2018-03-07 20:40:21 -0800

    add type head

---
+++ This的练习/this.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lee on 12/23/17.
+ */
+
+//
+const obj = {
+    team: 'jean',
+    number: 4,
+    age: 32,
+    out() {
+        console.log(this.number + this.age);
+    }
+}
+
+console.log(obj)
